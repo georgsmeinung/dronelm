e2d9dc1339cd3c88b62ff4c5746ae8efa18b6488
Jorge Nicolau <jenicolau@gmail.com>
2026-07-05 22:58:10 -0300

    Changed PoC commands

--- airsim-poc/my_hello_drone.py
+++ airsim-poc/my_hello_drone.py
@@ -45,55 +45,21 @@ client.takeoffAsync().join()
 state = client.getMultirotorState()
 print("state: %s" % pprint.pformat(state))
 
-airsim.wait_key('Press any key to move vehicle to (-10, 50, -10) at 5 m/s')
-client.moveToPositionAsync(-10, 50, -10, 5).join()
+airsim.wait_key('Press any key to move vehicle to (-10, 50, -50) at 10 m/s')
+client.moveToPositionAsync(-10, 50, -50, 10).join()
 
 client.hoverAsync().join()
 
 state = client.getMultirotorState()
 print("state: %s" % pprint.pformat(state))
 
-airsim.wait_key('Press any key to move vehicle to (0, 30, 0) at 10 m/s')
-client.moveToPositionAsync(0, 30, 0, 10).join()
+airsim.wait_key('Press any key to move vehicle to (40, 30, -30) at 10 m/s')
+client.moveToPositionAsync(40, 30, -30, 10).join()
 
 client.hoverAsync().join()
 
 state = client.getMultirotorState()
 print("state: %s" % pprint.pformat(state))
-
-
-airsim.wait_key('Press any key to take images')
-# get camera images from the car
-responses = client.simGetImages([
-    airsim.ImageRequest("0", airsim.ImageType.DepthVis),  #depth visualization image
-    airsim.ImageRequest("1", airsim.ImageType.DepthPerspective, True), #depth in perspective projection
-    airsim.ImageRequest("1", airsim.ImageType.Scene), #scene vision image in png format
-    airsim.ImageRequest("1", airsim.ImageType.Scene, False, False)])  #scene vision image in uncompressed RGBA array
-print('Retrieved images: %d' % len(responses))
-
-tmp_dir = os.path.join(tempfile.gettempdir(), "airsim_drone")
-print ("Saving images to %s" % tmp_dir)
-try:
-    os.makedirs(tmp_dir)
-except OSError:
-    if not os.path.isdir(tmp_dir):
-        raise
-
-for idx, response in enumerate(responses):
-
-    filename = os.path.join(tmp_dir, str(idx))
-
-    if response.pixels_as_float:
-        print("Type %d, size %d" % (response.image_type, len(response.image_data_float)))
-        airsim.write_pfm(os.path.normpath(filename + '.pfm'), airsim.get_pfm_array(response))
-    elif response.compress: #png format
-        print("Type %d, size %d" % (response.image_type, len(response.image_data_uint8)))
-        airsim.write_file(os.path.normpath(filename + '.png'), response.image_data_uint8)
-    else: #uncompressed array
-        print("Type %d, size %d" % (response.image_type, len(response.image_data_uint8)))
-        img1d = np.fromstring(response.image_data_uint8, dtype=np.uint8) # get numpy array
-        img_rgb = img1d.reshape(response.height, response.width, 3) # reshape array to 4 channel image array H X W X 3
-        cv2.imwrite(os.path.normpath(filename + '.png'), img_rgb) # write to png
 
 airsim.wait_key('Press any key to move vehicle to (0, 0, 0) at 7 m/s')
 client.moveToPositionAsync(0, 0, 0, 7).join()
